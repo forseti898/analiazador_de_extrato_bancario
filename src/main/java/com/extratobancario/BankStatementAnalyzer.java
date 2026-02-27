4b2dddd2a9115bb0740ef86fb7f8639ef2ad6517
forseti898 <freitasrenato898@gmail.com>
2026-02-27 19:38:06 -0300

    primeiro commit do projeto

---
+++ src/main/java/com/extratobancario/BankStatementAnalyzer.java
@@ -0,0 +1,23 @@
+package com.extratobancario;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class BankStatementAnalyzer {
+    private static final String RESOURCES = "src/main/resources/";
+
+    public void analyze(final String fileName, final BankStatementParser bankStatementParser) throws IOException{
+        final Path path = Paths.get(RESOURCES + fileName);
+        final List<String> lines = Files.readAllLines(path);
+
+        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFrom(lines);
+
+        final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
+
+        
+    }
+
+}
